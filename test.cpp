3f692ba51d97f2eb6be431ba9e7870c91d8d6667
Zarlove <1625778971@qq.com>
2017-09-12 10:15:32 +0800

    test.cpp

---
+++ test.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main() {
+	void *m, *n, *N, *i, *j;
+	/*cin >> m >> n >> N >> i >> j;
+	int M = 1000000007;
+	unsigned int count[2][50][50] = {};
+	cout << count[1][1][1];
+	int q, p, r;
+	int np, cp;
+	for (q = 1; q <= N; q++) {
+		np = (q + 1) % 2;
+		cp = q % 2;
+		for (p = 0; p < m; p++) {
+			for (r = 0; r < n; r++) {
+				count[np][p][r] = ((p == 0 ? 1 : count[cp][p - 1][r]) % M + (p == m - 1 ? 1 : count[cp][p + 1][r]) % M + (r == 0 ? 1 : count[cp][p][r - 1]) % M + (r == n - 1 ? 1 : count[cp][p][r + 1]) % M) % 1000000007;
+			}
+		}
+	}*/
+	/*cout<<count[np][i][j];*/
+	getchar();
+	return 0;
+}
